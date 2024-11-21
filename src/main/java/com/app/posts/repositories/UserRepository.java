c10f650d06969ed2a77e026f0347ef0b1d3ab526
Alexander Flores <Alexander_Flores_Solar@hotmail.com>
2024-11-21 09:07:49 -0500

    Adding repository stereotype

--- src/main/java/com/app/posts/repositories/UserRepository.java
+++ src/main/java/com/app/posts/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.app.posts.repositories;
 
 import com.app.posts.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 }
