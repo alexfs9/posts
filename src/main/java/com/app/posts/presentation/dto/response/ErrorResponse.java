55e6e95d64d5d0e0fd4997bcfc240bdcdd5ac2af
Alexander Flores <Alexander_Flores_Solar@hotmail.com>
2024-12-23 19:20:11 -0500

    Adding JSON property order

--- src/main/java/com/app/posts/presentation/dto/response/ErrorResponse.java
+++ src/main/java/com/app/posts/presentation/dto/response/ErrorResponse.java
@@ -1,7 +1,10 @@
 package com.app.posts.presentation.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.time.LocalDateTime;
 
+@JsonPropertyOrder({"time", "message", "status"})
 public record ErrorResponse(
         LocalDateTime time,
         String message,
