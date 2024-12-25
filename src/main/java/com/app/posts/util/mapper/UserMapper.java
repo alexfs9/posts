5152a747dabc3d34a7ff341230f33145c04f8584
Alexander Flores <Alexander_Flores_Solar@hotmail.com>
2024-12-25 13:47:11 -0500

    Fix to issue between Mapstruct and Lombok

--- src/main/java/com/app/posts/util/mapper/UserMapper.java
+++ src/main/java/com/app/posts/util/mapper/UserMapper.java
@@ -3,22 +3,12 @@ package com.app.posts.util.mapper;
 import com.app.posts.persistence.entity.UserEntity;
 import com.app.posts.presentation.dto.UserDTO;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(uses = {PostMapper.class, RoleMapper.class})
+@Mapper(componentModel = "spring", uses = PostMapper.class)
 public interface UserMapper {
 
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
-    @Mapping(target = "id", ignore = true)
-    UserEntity userDTOToUserEntity(UserDTO userDTO);
-
-    @Mapping(target = "password", ignore = true)
-    @Mapping(target = "isEnabled", ignore = true)
-    @Mapping(target = "accountNonExpired", ignore = true)
-    @Mapping(target = "credentialsNonExpired", ignore = true)
-    @Mapping(target = "accountNonLocked", ignore = true)
-    @Mapping(target = "roles", ignore = true)
-    UserDTO userEntityToUserDTO(UserEntity userEntity);
+    UserDTO toDto(UserEntity userEntity);
 }
