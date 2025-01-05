6a7068b815600ce8687e5cdb03ed015166b1d86e
Alexander Flores <Alexander_Flores_Solar@hotmail.com>
2025-01-05 13:15:35 -0500

    Deleting Validated annotation

--- src/main/java/com/app/posts/presentation/dto/request/auth/role/CreateRoleRequest.java
+++ src/main/java/com/app/posts/presentation/dto/request/auth/role/CreateRoleRequest.java
@@ -1,11 +1,9 @@
 package com.app.posts.presentation.dto.request.auth.role;
 
 import jakarta.validation.constraints.Size;
-import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 
-@Validated
 public record CreateRoleRequest(
         @Size(max = 1, message = "Basic users cannot have more than 1 role") List<String> roleNames
 ) {
